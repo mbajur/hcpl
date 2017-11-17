a64ffb677592e75e3f2aacd67a9a9b6cfd91cd94
Michal Bajur <mbajur@gmail.com>
2017-11-18 00:14:46 +0100

    remove creating first user from entrypoint

--- docker/production/entrypoint.sh
+++ docker/production/entrypoint.sh
@@ -7,9 +7,6 @@ bundle exec rake db:create > /dev/null 2>&1 || true
 echo "Migrating database..."
 bundle exec rake db:migrate
 
-echo "Creating first user..."
-bundle exec rake crm:first_user
-
 echo "Removing server pid if exists..."
 rm -f /app/tmp/pids/server.pid
 
