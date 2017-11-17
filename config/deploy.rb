8004fced7047861d82c8ae47a81a325a8c96f0bc
Michal Bajur <mbajur@gmail.com>
2017-11-17 23:20:54 +0100

    fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,22 +9,22 @@ require 'mina/git'
 #   repository   - Git repo to clone from. (needed by mina/git)
 #   branch       - Branch name to deploy. (needed by mina/git)
 
-set :application_name, 'foobar'
-set :domain, 'foobar.com'
-set :deploy_to, '/var/www/foobar.com'
-set :repository, 'git://...'
+set :application_name, 'hcpl'
+set :domain, 'forum.hard-core.pl'
+set :deploy_to, '/home/mbajur/hcpl'
+set :repository, 'git@github.com:mbajur/hcpl.git'
 set :branch, 'master'
 
 # Optional settings:
-#   set :user, 'foobar'          # Username in the server to SSH to.
+set :user, 'mbajur'          # Username in the server to SSH to.
 #   set :port, '30000'           # SSH port number.
-#   set :forward_agent, true     # SSH forward_agent.
+set :forward_agent, true     # SSH forward_agent.
 
 # Shared dirs and files will be symlinked into the app-folder by the 'deploy:link_shared_paths' step.
 # Some plugins already add folders to shared_dirs like `mina/rails` add `public/assets`, `vendor/bundle` and many more
 # run `mina -d` to see all folders and files already included in `shared_dirs` and `shared_files`
-# set :shared_dirs, fetch(:shared_dirs, []).push('public/assets')
-# set :shared_files, fetch(:shared_files, []).push('config/database.yml', 'config/secrets.yml')
+set :shared_dirs, fetch(:shared_dirs, []).push('public/assets')
+set :shared_files, fetch(:shared_files, []).push('config/database.yml', 'config/secrets.yml', 'vendor/env.web', 'secrets.env')
 
 # This task is the environment that is loaded for all remote run commands, such as
 # `mina deploy` or `mina rake`.
@@ -40,11 +40,26 @@ end
 # Put any custom commands you need to run at setup
 # All paths in `shared_dirs` and `shared_paths` will be created on their own.
 task :setup do
-  # command %{rbenv install 2.3.0 --skip-existing}
+  # queue! %[mkdir -p "#{deploy_to}/#{shared_path}/log"]
+  # queue! %[chmod g+rx,u+rwx "#{deploy_to}/#{shared_path}/log"]
+
+  # queue! %[mkdir -p "#{deploy_to}/#{shared_path}/config"]
+  # queue! %[chmod g+rx,u+rwx "#{deploy_to}/#{shared_path}/config"]
+
+  # queue! %[touch "#{deploy_to}/#{shared_path}/config/database.yml"]
+  # # queue! %[touch "#{deploy_to}/#{shared_path}/config/secrets.yml"]
+  # queue  %[echo "-----> Be sure to edit '#{deploy_to}/#{shared_path}/config/database.yml' and 'secrets.yml'."]
+
+  # # Docker dotenv files
+  # queue! %[mkdir -p "#{deploy_to}/#{shared_path}/vendor"]
+  # queue! %[touch "#{deploy_to}/#{shared_path}/vendor/env.web"]
+  # queue! %[touch "#{deploy_to}/#{shared_path}/vendor/env.db"]
 end
 
 desc "Deploys the current version to the server."
 task :deploy do
+  # set :volumes, %Q{VOLUMES_FROM="#{fetch(:deploy_to)}/#{fetch(:shared_path)}/config:/config" }
+
   # uncomment this line to make sure you pushed your local branch to the remote origin
   # invoke :'git:ensure_pushed'
   deploy do
@@ -52,15 +67,17 @@ task :deploy do
     # instance of your project.
     invoke :'git:clone'
     invoke :'deploy:link_shared_paths'
-    invoke :'bundle:install'
-    invoke :'rails:db_migrate'
-    invoke :'rails:assets_precompile'
+    # invoke :'bundle:install'
+    # invoke :'rails:db_migrate'
+    # invoke :'rails:assets_precompile'
     invoke :'deploy:cleanup'
 
     on :launch do
       in_path(fetch(:current_path)) do
-        command %{mkdir -p tmp/}
-        command %{touch tmp/restart.txt}
+        comment %{"Building docker container."}
+        command %{docker-compose -f docker/production/docker-compose.yml build}
+        # command %{mkdir -p tmp/}
+        # command %{touch tmp/restart.txt}
       end
     end
   end
