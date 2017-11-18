0ae0c2645ac2685748493b9d49288b101b50ffc1
Michal Bajur <mbajur@gmail.com>
2017-11-18 11:50:51 +0100

    hotfix

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,3 +4,5 @@ lock "~> 3.10.0"
 set :application, "hcpl"
 set :repo_url, 'git@github.com:mbajur/hcpl.git'
 set :deploy_to, "/mnt/disk/apps/#{fetch(:application)}"
+
+set :ssh_options, keys: ["config/deploy_id_rsa"] if File.exist?("config/deploy_id_rsa")
