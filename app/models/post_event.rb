2dc2a9473ac1432080823daca42363bafd9644ab
Michal Bajur <mbajur@gmail.com>
2017-11-20 09:42:20 +0100

    Display only upcoming events in sidebar

--- app/models/post_event.rb
+++ app/models/post_event.rb
@@ -2,4 +2,5 @@ class PostEvent < ApplicationRecord
   belongs_to :post
 
   scope :upcoming_first, -> { order(beginning_at: :asc) }
+  scope :upcoming, -> { where('beginning_at > ?', Time.zone.now.end_of_day) }
 end
