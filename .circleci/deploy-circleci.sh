d2973db1c43118e4e554d0590e474484197e3e21
Michal Bajur <mbajur@gmail.com>
2017-11-21 19:11:27 +0100

    fix

---
+++ .circleci/deploy-circleci.sh
@@ -0,0 +1,13 @@
+#!/bin/sh -eu
+
+cat <<EOF >> $HOME/.ssh/config
+  User mbajur
+  ForwardAgent yes
+EOF
+
+# Add the preferred key for getting GitHub Permission
+# See https://circleci.com/gh/masutaka/masutaka.net/edit#checkout
+eval $(ssh-agent)
+ssh-add
+
+bundle exec cap produdction deploy
