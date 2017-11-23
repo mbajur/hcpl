79898a21349493e33393153f98e005b4b24ec087
Michal Bajur <mbajur@gmail.com>
2017-11-23 17:23:35 +0100

    Make it able to search events #9

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,31 +3,27 @@ class EventsController < ApplicationController
   layout 'with_sidebar'
 
   def index
-    @posts = fetch_posts.upcoming_events.page(params[:page]).per(50)
-    @posts_months = @posts.group_by { |p| p.event.beginning_at.beginning_of_month }
+    @events = fetch_events.upcoming.upcoming_first
+
+    @events_months = @events.group_by { |e| e.beginning_at.beginning_of_month }
     @cities = cities_stats_for(PostEvent.upcoming)
   end
 
   def past
-    @posts = fetch_posts.past_events.page(params[:page]).per(50)
-    @posts_months = @posts.group_by { |p| p.event.beginning_at.beginning_of_month }
+    @events = fetch_events.past
+    @events_months = @events.group_by { |e| e.beginning_at.beginning_of_month }
     @cities = cities_stats_for(PostEvent.past)
   end
 
   private
 
-  def fetch_posts
-    res = Post
-            .includes(post_includes)
-            .tagged_with('Wydarzenia')
+  def fetch_events
+    res = PostEvent.includes(:post)
+                   .page(params[:page])
 
     res = res.search(params[:q]) if params[:q].present?
 
     res
-  end
-
-  def post_includes
-    [:tags, :votes, :user, :bookmarks, :event]
   end
 
   def cities_stats_for(col)
@@ -35,15 +31,11 @@ class EventsController < ApplicationController
     cities = col.group(:city).count
 
     cities.keys.each do |k|
-      res << {
-        name: k,
-        count: cities[k]
-      }
+      res << { name: k, count: cities[k] }
     end
 
-    res
-      .sort! { |a,b| b[:count] <=> a[:count] }
-      .reject! { |c| c[:name].nil? }
+    res.sort! { |a,b| b[:count] <=> a[:count] }
+       .reject! { |c| c[:name].nil? }
 
     sum = res.sum { |r| r[:count] }
     res.each { |c| c[:percent] = c[:count].to_f / sum.to_f * 100.0 }
--- app/models/post_event.rb
+++ app/models/post_event.rb
@@ -1,9 +1,13 @@
 class PostEvent < ApplicationRecord
+  include PgSearch
+
   belongs_to :post
 
   scope :upcoming_first, -> { order(beginning_at: :asc) }
   scope :upcoming, -> { where('beginning_at > ?', Time.zone.now.beginning_of_day) }
   scope :past, -> { where('beginning_at < ?', Time.zone.now.beginning_of_day) }
+
+  pg_search_scope :search, against: :city, associated_against: { post: :title }, using: { tsearch: { prefix: true } }
 
   def can_be_synced?
     !synced_at.present? || (synced_at.present? && synced_at < 10.minutes.ago)
