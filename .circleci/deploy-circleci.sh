4d7ce688d14d88acf72fed457409c95dc165621c
Michal Bajur <mbajur@gmail.com>
2017-11-26 22:58:43 +0100

    hotfix

--- .circleci/deploy-circleci.sh
+++ .circleci/deploy-circleci.sh
@@ -1,14 +1,14 @@
 #!/bin/sh -eu
 
-cat <<EOF >> $HOME/.ssh/config
-  User mbajur
-  ForwardAgent yes
-EOF
+# cat <<EOF >> $HOME/.ssh/config
+#   User mbajur
+#   ForwardAgent yes
+# EOF
 
 # Add the preferred key for getting GitHub Permission
 # See https://circleci.com/gh/masutaka/masutaka.net/edit#checkout
-eval $(ssh-agent)
-ssh-add
+# eval $(ssh-agent)
+# ssh-add
 
 git remote add production dokku@dokku.hcxp.co:hcpl
 git push production master
