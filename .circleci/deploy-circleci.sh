cd356be292e4641afd56e67db8ab28287a4db66d
Michal Bajur <mbajur@gmail.com>
2017-11-26 23:02:53 +0100

    hotfix

--- .circleci/deploy-circleci.sh
+++ .circleci/deploy-circleci.sh
@@ -10,5 +10,9 @@
 # eval $(ssh-agent)
 # ssh-add
 
+# Fix host authenticity for host
+# taken from https://discuss.circleci.com/t/capistrano-and-ssh-host-authenticity/13586/6
+ssh-keyscan dokku.hcxp.co >> ~/.ssh/known_hosts
+
 git remote add production dokku@dokku.hcxp.co:hcpl
 git push production master
