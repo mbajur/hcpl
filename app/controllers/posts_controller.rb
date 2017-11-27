aaaefb231b59027486c3bfc7e6afc8c8dcd8c6a0
Michal Bajur <mbajur@gmail.com>
2017-11-27 20:06:33 +0100

    Fix #17

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -53,7 +53,8 @@ class PostsController < ApplicationController
   end
 
   def create
-    redirect_to_post_if_exists
+    res = redirect_to_post_if_exists
+    return false unless res
 
     @post = Posts::Create.run(
       posts_create_params.merge!(user: current_user)
@@ -81,13 +82,13 @@ class PostsController < ApplicationController
 
   # If post with given link exists, redirect user to it with proper notice
   def redirect_to_post_if_exists
-    return unless posts_create_params[:link].present?
+    return true unless posts_create_params[:link].present?
 
     existing_post = Post.find_by(link: posts_create_params[:link])
-    return unless existing_post.present?
+    return true unless existing_post.present?
 
-    redirect_to existing_post.path
-    return
+    redirect_to existing_post.path, notice: 'Link był już dodany wcześniej. Zostałeś do niego przekierowany.'
+    return false
   end
 
   def post_includes
