b7dc2c1d04103096dcef47fa58c00d3643f25cf2
Michal Bajur <mbajur@gmail.com>
2017-11-30 14:56:48 +0100

    Cache link data fetcher, close #11

--- app/interactions/fetch_link_data.rb
+++ app/interactions/fetch_link_data.rb
@@ -4,13 +4,26 @@ class FetchLinkData < ActiveInteraction::Base
   validates :link, presence: true, url: { message: 'wydaje się być niepoprawny' }
 
   def execute
-    page       = MetaInspector.new(inputs[:link])
-    classifier = PageClassifier::Perform.new(page).call
-
     {
-      title:      page.best_title,
-      media_type: classifier.media_type,
-      media_guid: classifier.media_guid
+      title:      data[:page].best_title,
+      media_type: data[:classifier].media_type,
+      media_guid: data[:classifier].media_guid
     }
+  end
+
+  private
+
+  def cache_key
+    md5 = Digest::MD5.new.hexdigest inputs[:link]
+    ['fetch_link_data', md5].join('/')
+  end
+
+  def data
+    Rails.cache.fetch(cache_key, expires_in: 10.minutes) do
+      page       = MetaInspector.new(inputs[:link])
+      classifier = PageClassifier::Perform.new(page).call
+
+      { page: page, classifier: classifier }
+    end
   end
 end
