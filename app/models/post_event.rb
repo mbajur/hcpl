acd519435986e8f14b17a750099ed7b9a53f224b
Michal Bajur <mbajur@gmail.com>
2017-12-24 10:43:03 +0100

    Less restrictive hot events widget on events page

--- app/models/post_event.rb
+++ app/models/post_event.rb
@@ -7,9 +7,11 @@ class PostEvent < ApplicationRecord
 
   scope :upcoming_first, -> { order(beginning_at: :asc) }
   scope :upcoming, -> { where('beginning_at > ?', Time.zone.now.beginning_of_day) }
+  scope :upcoming_4_months, -> { where('beginning_at > ?', Time.zone.now.beginning_of_day).where('beginning_at <= ?', Time.zone.now + 4.months) }
   scope :past, -> { where('beginning_at < ?', Time.zone.now.beginning_of_day) }
   scope :this_week, -> { where('beginning_at > ?', Time.zone.now.beginning_of_week).where('beginning_at <= ?', Time.zone.now.end_of_week) }
-  scope :upcoming_hot, -> { where('beginning_at > ?', Time.zone.now.beginning_of_day).where('beginning_at <= ?', Time.zone.now + 15.days).where(is_hot: true) }
+  scope :upcoming_hot, -> { where('beginning_at > ?', Time.zone.now.beginning_of_day).where('beginning_at <= ?', Time.zone.now + 25.days).where(is_hot: true) }
+  scope :hot, -> { where(is_hot: true) }
 
   pg_search_scope :search, against: :city, associated_against: { post: :title }, using: { tsearch: { prefix: true } }
 
