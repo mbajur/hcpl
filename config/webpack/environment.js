8b1d0b43bff56daf12adc9e779ebab04c647a5a9
Michal Bajur <mbajur@gmail.com>
2017-12-26 10:03:12 +0100

    Fixes

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -3,11 +3,6 @@ const vuxLoader = require('vux-loader')
 
 // module.exports = environment
 
-console.log('-----> DEBUG:')
-// console.log(environment)
-console.log(environment.plugins)
-environment.plugins.push({})
-
 module.exports = vuxLoader.merge(environment, {
   plugins: ['vux-ui']
 })
